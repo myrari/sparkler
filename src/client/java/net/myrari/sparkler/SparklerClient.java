3b847a59df6f4306e58e3aa0567ad83228c3c39a
Myra Roberts <myra@myrari.net>
2025-12-25 13:32:46 -0700

    removed server-specific things, mod is clientside only

--- src/client/java/net/myrari/sparkler/SparklerClient.java
+++ src/client/java/net/myrari/sparkler/SparklerClient.java
@@ -1,10 +1,32 @@
 package net.myrari.sparkler;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 
 public class SparklerClient implements ClientModInitializer {
+	public static final String MOD_ID = "sparkler";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		// This entrypoint is suitable for setting up client-specific logic, such as
+		// rendering.
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(Commands.literal("test").executes(ctx -> {
+				LOGGER.info("Called /test!");
+				ctx.getSource().sendSuccess(() -> Component.literal("Called /test"), false);
+				return 1;
+			}));
+		});
 	}
 }
