3a904f9d5f5a27a89c45d8054919d4359a3ee894
Myra Roberts <myra@myrari.net>
2025-12-25 13:54:38 -0700

    started setting up http things

--- src/client/java/net/myrari/sparkler/SparklerClient.java
+++ src/client/java/net/myrari/sparkler/SparklerClient.java
@@ -1,12 +1,21 @@
 package net.myrari.sparkler;
 
+import java.net.http.HttpClient;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.mojang.brigadier.context.CommandContext;
+
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.commands.Commands;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.network.chat.Component;
+
+class SparklerConfig {
+
+}
 
 public class SparklerClient implements ClientModInitializer {
 	public static final String MOD_ID = "sparkler";
@@ -16,17 +25,21 @@ public class SparklerClient implements ClientModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	private static int executeSparkleCmd(CommandContext<FabricClientCommandSource> ctx, HttpClient httpClient) {
+		LOGGER.info("Called /sparkle!");
+		ctx.getSource().sendFeedback(Component.literal("Called /sparkle"));
+		return 1;
+	}
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as
 		// rendering.
 
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-			dispatcher.register(Commands.literal("test").executes(ctx -> {
-				LOGGER.info("Called /test!");
-				ctx.getSource().sendSuccess(() -> Component.literal("Called /test"), false);
-				return 1;
-			}));
+		HttpClient httpClient = HttpClient.newHttpClient();
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
+			dispatcher.register(ClientCommandManager.literal("sparkle").executes(ctx -> executeSparkleCmd(ctx, httpClient)));
 		});
 	}
 }
