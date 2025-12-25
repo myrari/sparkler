91f8103ca20f577af21503805c9394f34c07ca26
Myra Roberts <myra@myrari.net>
2025-12-25 13:04:49 -0700

    Initial commit

---
+++ src/client/java/net/myrari/sparkler/SparklerClient.java
@@ -0,0 +1,10 @@
+package net.myrari.sparkler;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SparklerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
