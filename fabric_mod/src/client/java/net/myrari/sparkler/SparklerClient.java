7b04b73c13cad1be2d80dd95e6445c94f6c0c1b9
Myra Roberts <myra@myrari.net>
2025-12-25 15:22:36 -0700

    minor cleanup

--- fabric_mod/src/client/java/net/myrari/sparkler/SparklerClient.java
+++ fabric_mod/src/client/java/net/myrari/sparkler/SparklerClient.java
@@ -6,9 +6,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpRequest.BodyPublishers;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
