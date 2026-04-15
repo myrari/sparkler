927ce19cd2377add6b5746516a92972cf22f3551
Myra Roberts <myra@myrari.net>
2026-04-15 16:52:02 -0700

    start working on v2 :3

---
+++ v2/src/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const root = dirname(fileURLToPath(import.meta.url))
+
+const PORT = process.env.PORT;
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+/** MAIN ROUTES */
+
+app.get("/", (_req, res) => {
+    res.sendFile("index.html", {
+        root: root,
+    })
+});
+
+/** IMMEDIATE LOGIC */
+
+// start the server
+app.listen(PORT, () => {
+    console.info(`The server is running at http://localhost:${PORT}`);
+});
